cffa3983f9c9161c4aa7273a342618fb0eea66e1
Irakozeelysee <elyseeirakoze@gmail.com>
2023-09-22 20:09:40 +0000

    Execute a command

---
+++ alx-system_engineering-devops/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# Puppet manifest to kill a process named "killmenow" using pkill
+
+exec { 'killmenow':
+  command     => 'pkill killmenow',
+  path        => '/bin:/usr/bin',
+  refreshonly => true,
+  subscribe   => File['/path/to/killmenow_script'], # Replace with the actual path to your "killmenow" script
+}
